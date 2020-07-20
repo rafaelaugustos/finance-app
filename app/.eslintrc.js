9eb7d43328b699bb5fb5eb0938fd2f28691d5293
Rafael Augusto <rafaelaugusto.developer@gmail.com>
2020-07-20 18:07:33 -0300

    create a lint config

--- app/.eslintrc.js
+++ app/.eslintrc.js
@@ -1,4 +1,29 @@
 module.exports = {
-  root: true,
-  extends: '@react-native-community',
+  env: {
+    es2020: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "prettier",
+    "prettier/react",
+  ],
+  parser: "babel-eslint",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 11,
+    sourceType: "module",
+  },
+  plugins: ["react", "prettier"],
+  rules: {
+    "prettier/prettier": "error",
+    "react/jsx-filenamme-extension": ["warn", { extensions: [".jsx", ".js"] }],
+    "import/prefer-default-export": "off",
+    indent: ["error", "tab"],
+    "linebreak-style": ["error", "unix"],
+    quotes: ["error", "single"],
+    semi: ["error", "never"],
+  },
 };
