03c3e2e0bc1fd09630be6d0cadeaf63877e317d3
Rafael Augusto <rafaelaugusto.developer@gmail.com>
2020-07-20 21:16:41 -0300

    app/header show false

--- app/src/routes/index.js
+++ app/src/routes/index.js
@@ -7,7 +7,7 @@ import Overview from '../pages/App/Overview'
 const { Navigator, Screen } = createStackNavigator()
 
 const AuthRoutes = () => (
-	<Navigator>
+	<Navigator screenOptions={{ headerShown: false }}>
 		<Screen name="Overview" component={Overview} />
 		<Screen name="SignIn" component={SignIn} />
 	</Navigator>
